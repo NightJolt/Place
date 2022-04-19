789b7ffc88760655d2f1abd67f8eb6850c897128
NightJolt <mk.ff.dev@gmail.com>
2022-04-20 02:16:50 +0400

    update

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,51 +1,27 @@
 #include "../FunEngine2D/core/include/globals.h"
+#include "../FunEngine2D/core/include/vec2.h"
+#include "../FunEngine2D/core/include/_time.h"
+#include "../FunEngine2D/core/include/render/window_manager.h"
+#include "../FunEngine2D/core/include/networking/client.h"
+#include "../FunEngine2D/core/include/networking/server.h"
 
-int main() {
-    println("Working");
+int main () {
+    fun::winmgr::init(fun::winmgr::window_data_t("Place Client"));
+    auto* window = fun::winmgr::main_window;
+
+    fun::client_t client;
+    if (!client.connect("localhost", 8001)) exit(69);
+    
+    while (window->render.isOpen()) {
+        fun::time::recalculate();
+        fun::winmgr::update();
+
+        window->display(sf::Color::Black);
+    }
+
+    client.disconnect();
+
+    fun::winmgr::close();
 
     return 0;
-}
-
-// #include "FunEngine2D/core/include/globals.h"
-
-// #include "../FunEngine2D/"
-
-// #include "FunEngine2D/core/include/vec2.h"
-// #include "FunEngine2D/core/include/transform.h"
-// #include "FunEngine2D/core/include/render/window_manager.h"
-// #include "FunEngine2D/core/include/resources.h"
-// #include "FunEngine2D/core/include/_time.h"
-// #include "FunEngine2D/core/include/input.h"
-// #include "FunEngine2D/core/include/tools/debugger.h"
-// #include "FunEngine2D/core/include/render/shapes/particler.h"
-
-// #include "FunEngine2D/core/include/networking/client.h"
-// #include "FunEngine2D/core/include/networking/server.h"
-// #include "FunEngine2D/core/include/ecs/ecs.h"
-// #include "FunEngine2D/core/include/physics/physics.h"
-// #include "FunEngine2D/core/include/interact/interaction.h"
-// #include "FunEngine2D/core/include/interact/interactable.h"
-
-// int main () {
-//     fun::winmgr::init(fun::winmgr::window_data_t("Spaceships"));
-//     auto* window = fun::winmgr::main_window;
-    
-//     while (window->render.isOpen()) {
-//         fun::time::recalculate();
-//         fun::input::listen();
-//         fun::interaction::update();
-//         fun::winmgr::update();
-
-//         window->world_view.move((fun::input::keyboard_2d() * fun::vec2f_t(1, -1) * window->zoom * 200.f * fun::time::delta_time()).to_sf());
-
-        
-
-//         fun::debugger::display();
-
-//         window->display(sf::Color::Black);
-//     }
-
-//     fun::winmgr::close();
-
-//     return 0;
-// };
+};
---
+++ Server/main.cpp
@@ -0,0 +1,85 @@
+#include "../FunEngine2D/core/include/globals.h"
+#include "../FunEngine2D/core/include/vec2.h"
+#include "../FunEngine2D/core/include/_time.h"
+#include "../FunEngine2D/core/include/render/window_manager.h"
+#include "../FunEngine2D/core/include/networking/client.h"
+#include "../FunEngine2D/core/include/networking/server.h"
+#include "../FunEngine2D/core/include/_math.h"
+
+// namespace std {
+//     template <> struct hash<fun::vec2i_t> {
+//         size_t operator()(const fun::vec2i_t& v) const {
+//             return (v.x + v.y) * (v.x + v.y + 1) >> 1 + v.x;
+//         }
+//     };
+// }
+
+struct color_t {
+    color_t() : r(0), g(0), b(0) {}
+    color_t(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b) {}
+
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+};
+
+template <int8_t X, int8_t Y>
+class chunk_t {
+public:
+    inline color_t get_color(uint8_t x, uint8_t y) { return data[x][y]; }
+    inline void set_color(uint8_t x, uint8_t y, color_t color) { data[x][y] = color; }
+
+private:
+    color_t data[X][Y];
+};
+
+class canvas_t {
+public:
+    static constexpr uint8_t size_x = 16;
+    static constexpr uint8_t size_y = 16;
+
+    chunk_t <size_x, size_y>* get_chunk(int32_t x, int32_t y) {
+        const auto coord = fun::vec2i_t { (x < 0 ? (x * -1 - 1) : x), (y < 0 ? (y * -1 - 1) : y) };
+
+        return data[coord];
+    }
+
+    color_t get_color(int32_t x, int32_t y) {
+        return get_chunk(x, y)->get_color(fun::math::mod(x, size_x), fun::math::mod(y, size_y));
+    }
+
+    void set_color(int32_t x, int32_t y, color_t color) {
+        get_chunk(x, y)->set_color(fun::math::mod(x, size_x), fun::math::mod(y, size_y), color);
+    }
+
+private:
+    std::map <fun::vec2i_t, chunk_t <size_x, size_y>*> data;
+};
+
+int main () {
+    fun::winmgr::init(fun::winmgr::window_data_t("Place Server"));
+    auto* window = fun::winmgr::main_window;
+
+    // fun::server_t server;
+    // server.launch(8001);
+
+    canvas_t canvas;
+
+    canvas.set_color(3, 4, color_t(2, 2, 2));
+    println(canvas.get_color(3, 4).r);
+    
+    while (window->render.isOpen()) {
+        fun::time::recalculate();
+        fun::winmgr::update();
+
+        // server.listen();
+
+        window->display(sf::Color::Black);
+    }
+
+    // server.terminate();
+
+    fun::winmgr::close();
+
+    return 0;
+};
